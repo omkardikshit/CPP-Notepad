8fce6e35932aca748ecbd8147aad56e2e5153f85
Omkar Dixit <red.destroy27@gmail.com>
2017-06-10 17:59:50 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Press Enter To Exit \n";
+    system("color 0a");
+    float a;
+    cin >> a;
+    cin.get();
+    system("pause");
+    cin.ignore();
+    cin >>  a;
+    return 0;
+}
